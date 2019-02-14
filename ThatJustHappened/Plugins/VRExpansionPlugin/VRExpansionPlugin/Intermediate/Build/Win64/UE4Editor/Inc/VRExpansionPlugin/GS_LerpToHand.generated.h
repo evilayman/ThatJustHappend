adf34de1ec386b102f71407c3706e566dc48e4aa
kai <0ultrakai0@gmail.com>
2019-02-14 03:20:51 +0200

    VR Expansion Plugin (may not work yet)

---
+++ ThatJustHappened/Plugins/VRExpansionPlugin/VRExpansionPlugin/Intermediate/Build/Win64/UE4Editor/Inc/VRExpansionPlugin/GS_LerpToHand.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VREXPANSIONPLUGIN_GS_LerpToHand_generated_h
+#error "GS_LerpToHand.generated.h already included, missing '#pragma once' in GS_LerpToHand.h"
+#endif
+#define VREXPANSIONPLUGIN_GS_LerpToHand_generated_h
+
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_RPC_WRAPPERS
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_RPC_WRAPPERS_NO_PURE_DECLS
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUGS_LerpToHand(); \
+	friend struct Z_Construct_UClass_UGS_LerpToHand_Statics; \
+public: \
+	DECLARE_CLASS(UGS_LerpToHand, UVRGripScriptBase, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGS_LerpToHand)
+
+
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_INCLASS \
+private: \
+	static void StaticRegisterNativesUGS_LerpToHand(); \
+	friend struct Z_Construct_UClass_UGS_LerpToHand_Statics; \
+public: \
+	DECLARE_CLASS(UGS_LerpToHand, UVRGripScriptBase, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGS_LerpToHand)
+
+
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UGS_LerpToHand(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGS_LerpToHand) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGS_LerpToHand); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGS_LerpToHand); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGS_LerpToHand(UGS_LerpToHand&&); \
+	NO_API UGS_LerpToHand(const UGS_LerpToHand&); \
+public:
+
+
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGS_LerpToHand(UGS_LerpToHand&&); \
+	NO_API UGS_LerpToHand(const UGS_LerpToHand&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGS_LerpToHand); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGS_LerpToHand); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGS_LerpToHand)
+
+
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_PRIVATE_PROPERTY_OFFSET
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_27_PROLOG
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_PRIVATE_PROPERTY_OFFSET \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_RPC_WRAPPERS \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_INCLASS \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_PRIVATE_PROPERTY_OFFSET \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_RPC_WRAPPERS_NO_PURE_DECLS \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_INCLASS_NO_PURE_DECLS \
+	ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_30_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ThatJustHappened_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h
+
+
+#define FOREACH_ENUM_EVRLERPINTERPOLATIONMODE(op) \
+	op(EVRLerpInterpolationMode::QuatInterp) \
+	op(EVRLerpInterpolationMode::EulerInterp) \
+	op(EVRLerpInterpolationMode::DualQuatInterp) 
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ ThatJustHappened/Source/ThatJustHappened/ThatJustHappened.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ThatJustHappened.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThatJustHappened, "ThatJustHappened" );
